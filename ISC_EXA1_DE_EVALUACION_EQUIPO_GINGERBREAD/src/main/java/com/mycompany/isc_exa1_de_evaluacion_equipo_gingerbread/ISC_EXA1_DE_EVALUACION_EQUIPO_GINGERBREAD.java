d0435fe4bc624e83c09428154742f7068eb696bb
Cokthegold <143568521+Cokthegold@users.noreply.github.com>
2023-10-03 00:00:06 -0600

    Add files via upload

---
+++ ISC_EXA1_DE_EVALUACION_EQUIPO_GINGERBREAD/src/main/java/com/mycompany/isc_exa1_de_evaluacion_equipo_gingerbread/ISC_EXA1_DE_EVALUACION_EQUIPO_GINGERBREAD.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.isc_exa1_de_evaluacion_equipo_gingerbread;
+
+/**
+ *
+ * @author Usuario
+ */
+public class ISC_EXA1_DE_EVALUACION_EQUIPO_GINGERBREAD {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+system.out.println("Hola Mundo");
+
+
+}
